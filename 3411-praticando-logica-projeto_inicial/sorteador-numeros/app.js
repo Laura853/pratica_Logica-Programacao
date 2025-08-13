d9256a206e0cc064017b6061f99def4d5b0ab2bd
laura853 <lauralopess2006@gmail.com>
2025-08-13 15:06:56 -0300

    exercicio 1 projeto sorteador de números

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear () {
+    let quantidadeDeNumeros =  document.getElementById("quantidade").value;
+    let doNumero =  document.getElementById("de").value;
+    let ateONumero =  document.getElementById("ate").value;
+
+    alert(`Quantidade de números : ${quantidadeDeNumeros}`);
+    alert(`Do número : ${doNumero}`);
+    alert(`Até o número : ${ateONumero}`);
+}
